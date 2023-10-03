310fec04fe4241639c4b94f7b66ba5a7802fceca
Adesh-code <adesh@code-b.in>
2023-10-03 18:16:39 +0530

    starting

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,6 @@
-import './App.css';
-import Button from './components/Button/Button';
-import { useColor } from './hooks/useColor';
-import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
-
+import "./App.css";
 function App() {
-  const theme = useColor({});
-  return (
-    <div className="App">
-      <Button
-        backgroundColor={theme.Primary}
-        className="rounded-full"
-        textColor={theme.White}
-        title="Submit"
-        onPress={() => console.log('Button Pressed')}
-        leftIcon={faArrowRight}
-      />
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
